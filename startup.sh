aec8d23fcd84837af291c6cbccd7f2ece663a0bf
Olavio Lacerda <olavio.lacerda@hotmail.com>
2020-06-27 23:10:00 -0300

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -81,8 +81,8 @@ cd "$NVM_DIR"
 git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
 ) && \. "$NVM_DIR/nvm.sh"
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+echo 'export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> .zshrc
 
 source ~/.zshrc
 nvm --version
